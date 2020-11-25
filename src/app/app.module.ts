2ad92b6a59cf90fc9db3c2bb8892ceca20ffc55b
jcgouleau <jean-charles.gouleau@savoye.com>
2020-11-25 22:14:03 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ErrorComponent } from './error/error.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    ProductCreateComponent,
+    ProductListComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatToolbarModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
